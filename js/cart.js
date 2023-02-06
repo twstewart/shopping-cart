7e142dbff35b89adc9beb3cb8f5e1e1f9965880c
tdubsxc <trdubs90@gmail.com>
2023-02-05 19:14:45 -0500

    Setup functions to calculate the total price and number of cart items.

--- js/cart.js
+++ js/cart.js
@@ -2,9 +2,13 @@ import storeData from "../data/store-data.json";
 import { currencyFormatter } from "./utils";
 
 const cartItemTemplate = document.querySelector("#cart-item-template");
+const storeCartContainer = document.querySelector("#store-cart-container");
 const cartBtn = document.querySelector("[data-cart-button]");
 const cartItemsWrapper = document.querySelector("[data-cart-items-wrapper]");
 const cartItemsEl = document.querySelector("[data-cart-items]");
+
+const numOfCartItemsEl = document.querySelector("[data-cart-num-items]");
+const cartItemsTotalEl = document.querySelector("[data-cart-items-total]");
 
 let cart = [];
 
@@ -51,6 +55,39 @@ function renderCart() {
 
     cartItemsEl.append(cartItemEl);
   });
+
+  calcTotalItemsInCart();
+  calcTotalPriceOfItems();
+}
+
+function calcTotalItemsInCart() {
+  let totalCartItems = 0;
+
+  cart.forEach((cartItemEntry) => {
+    if (cartItemEntry.quantity >= 1) {
+      totalCartItems += cartItemEntry.quantity;
+    } else {
+      totalCartItems += 1;
+    }
+  });
+
+  numOfCartItemsEl.textContent = totalCartItems;
+}
+
+function calcTotalPriceOfItems() {
+  const totalPriceCents = cart.reduce((sum, cartItemEntry) => {
+    const cartItem = storeData.find(({ id }) => id === cartItemEntry.id);
+
+    if (cartItemEntry.quantity) {
+      sum += cartItemEntry.quantity * cartItem.priceCents;
+    } else {
+      sum += cartItem.priceCents;
+    }
+
+    return sum;
+  }, 0);
+
+  cartItemsTotalEl.textContent = currencyFormatter(totalPriceCents / 100);
 }
 
 export default function setupCart() {}
